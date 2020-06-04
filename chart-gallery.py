ef62e576e325f731bb254f60dd2b793ded786b35
alexkuvshinoff <45110174+alexkuvshinoff@users.noreply.github.com>
2020-06-04 19:48:39 -0400

    created pie chart

--- chart-gallery.py
+++ chart-gallery.py
@@ -1,2 +1,22 @@
 
-print("hello")
+# three_charts.py
+
+#
+# CHART 1 (PIE)
+#
+
+import matplotlib.pyplot as plt
+
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+labels = ["Company X", "Company Y", "Company Z"]
+sizes = [.55, .30, .15]
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
+ax1.axis("equal")  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+plt.show() # need to explicitly "show" the chart window
+
+print("----------------")
+print("GENERATING PIE CHART...")
+print(pie_data) # TODO: create a pie chart based on the pie_data
